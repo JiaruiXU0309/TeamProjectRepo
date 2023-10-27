1786bf166b91d2456e7d44a1dba4852a3c09f3bf
JiaruiXU0309 <202383930026@nuist.edu.cn>
2023-10-27 21:19:47 +0800

    Adding properties of house

--- TeamProject/src/House.java
+++ TeamProject/src/House.java
@@ -1,2 +1,14 @@
 public class House {
+    private Gift[] gifts;
+    int total = 0;
+
+    public House(int total) {
+        gifts = new Gift[total];
+    }
+    private boolean isFull() {
+        return total == gifts.length;
+    }
+    private boolean isEmpty(){
+        return total == 0;
+    }
 }
