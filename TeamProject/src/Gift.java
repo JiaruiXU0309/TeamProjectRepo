abb8e8cdef87aa557ff5fe324d62a8bce0a5c232
YiningSUN-0410 <147826016+YiningSUN-0410@users.noreply.github.com>
2023-10-27 21:19:14 +0800

    Adding Gift class

--- TeamProject/src/Gift.java
+++ TeamProject/src/Gift.java
@@ -1,5 +1,12 @@
 public class Gift {
     private String name;
     private String wrapper;
+    private String shape;
     private float prize;
+    private String location;
+    public Gift(String name,String wrapper,String shape,float prize,String location){
+
+
+    }
+
 }
