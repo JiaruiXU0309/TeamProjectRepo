464a0c3e9e0bdafb864e9a549a50fdd72b3613d7
JiaruiXU0309 <202383930026@nuist.edu.cn>
2023-11-26 21:08:27 +0800

    Adding prologue and character menu in Drive class

--- TeamProject/src/Driver.java
+++ TeamProject/src/Driver.java
@@ -1,2 +1,40 @@
+import java.util.Scanner;
 public class Driver {
+    private Scanner input = new Scanner(System.in);
+    private House house;
+
+    public static void main(String[] args) {
+        new Driver();
+    }
+
+    public Driver() {
+        processOrder();
+
+    }
+
+    private void processOrder(){
+        //prologue开场白
+        System.out.print("Christmas is coming." +
+                "Each family member have prepared gifts for each other." +
+                "Now please control each character to hide their gifts for each other.");
+        System.out.print("Which character do you want to control now? :)");
+        chooseCharacterMenu();
+
+        int userCharacter = input.nextInt();
+
+    }
+
+        private int chooseCharacterMenu(){
+            System.out.print("""
+               Character Menu
+               ---------
+                  1) Father
+                  2) Mother
+                  3) Eldest child
+                  4) Youngest child
+                  0) Exit
+               ==>> """);
+            int option = input.nextInt();
+            return option;
+        }
 }
