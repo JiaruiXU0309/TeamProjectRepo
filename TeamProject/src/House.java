731c71a9f27f8d9ae55443cdb84cbd3fe86143bd
JiaruiXU0309 <202383930026@nuist.edu.cn>
2023-11-26 20:34:54 +0800

    Adding methods of House class

--- TeamProject/src/House.java
+++ TeamProject/src/House.java
@@ -5,6 +5,7 @@ public class House {
     public House(int total) {
         gifts = new Gift[total];
     }
+//为House添加gift的数量
 
     private boolean isFull() {
         return total == gifts.length;
@@ -17,10 +18,35 @@ public class House {
     public boolean add(Gift gift) {
         if (isFull()) {
             return false;
-        } else {
+        }
+        else {
             gifts[total] = gift;
             total++;
             return true;
+        }
+    }
+
+    public boolean reduce(Gift gift){
+        if (isEmpty()) {
+            return false;
+        }
+        else {
+            gifts[total] = gift;
+            total--;
+            return true;
+        }
+    }
+
+    public String listGift() {
+        if (isEmpty()) {
+            return "No gifts here";
+        }
+        else{
+            String listOfGifts = "";
+            for(int i = 0; i < total; i++){
+                listOfGifts+= i + ": " + gifts[i] + "\n";
+            }
+            return listOfGifts;
         }
     }
 }
